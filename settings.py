367135ad61063257bc2358be5c18ae730e6ab23b
George-Daniel Gagiu <george.daniel.gagiu@gmail.com>
2024-07-22 19:32:35 +0300

    Update settings configuration

--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
-from dotenv import dotenv_values
+import os
+
+from dotenv import load_dotenv
 
 from utils.conversions import parse_int
-
 
 class Settings:
     @property
@@ -20,10 +21,11 @@ class Settings:
     def REDIS_URL(self):
         return self.__REDIS_URL
 
-    def __init__(self, config: dict[str, str | None]) -> None:
-        self.__EXTERNAL_API_ROOT = config.get('API_ROOT', 'https://pokeapi.co/api/v2')
-        self.__BERRIES_CACHE_TTL = parse_int(config.get('BERRIES_CACHE_TTL'), 86400)
-        self.__BERRIES_LOCK_TIMEOUT = parse_int(config.get('BERRIES_LOCK_TIMEOUT'), 300)
-        self.__REDIS_URL = config.get('REDIS_URL', 'redis://localhost:6379/0')
+    def __init__(self, env_path: str = '.env') -> None:
+        load_dotenv(env_path)
+        self.__EXTERNAL_API_ROOT = os.getenv('API_ROOT', 'https://pokeapi.co/api/v2')
+        self.__BERRIES_CACHE_TTL = parse_int(os.getenv('BERRIES_CACHE_TTL', '86400'))
+        self.__BERRIES_LOCK_TIMEOUT = parse_int(os.getenv('BERRIES_LOCK_TIMEOUT', '300'))
+        self.__REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
 
-app_settings = Settings(dotenv_values('.env'))
+app_settings = Settings()
